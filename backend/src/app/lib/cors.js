fbe3a8554fabdcde5761c4c8257e836f0b471404
wat6714522 <wat6714522@gmail.com>
2026-09-12 13:04:58 +0700

    add(): cors support

---
+++ backend/src/app/lib/cors.js
@@ -0,0 +1,9 @@
+const corsHeaders = {
+  "Access-Control-Allow-Credentials": true,
+  "Access-Control-Allow-Origin": "http://localhost:5173",
+  "Access-Control-Allow-Methos": "GET, POST, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+  "Access-Control-Max-Age": "86400",
+};
+
+export default corsHeaders;
